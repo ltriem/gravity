e89ef3dac344f83fe1f4d700846f7b405b0fc1d0
ltriem <ltriem@yandex.ru>
2019-10-15 00:49:37 +0300

    add-points

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,56 @@
+let halfX; let halfY; let width; let height;
+
+const lines = [];
+const linesNumber = 4;
+
+const vertices = 100;
+const radius = 200;
+const color = red;
+
+for (let i = 0; i < vertices; i++) {
+    const point = {
+        x: Math.cos(i/vertices * Math.PI*2),
+        y: Math.sin(i/vertices * Math.PI*2),
+
+    };
+    lines.push(points);
+};
+
+console.log(lines);
+
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
 document.body.appendChild(canvas);
 
 Sizing();
+
+function update() {
+
+}
+
+function render() {
+    ctx.clearRect(0, 0, width, height);
+    ctx.strokeStyle(color);
+
+    for (let i = 0; i < vertices; i++) {
+        ctx.beginPath();
+        ctx.moveTo();
+
+        ctx.stroke();
+    }
+
+}
+
+
+function raf() {
+
+    update();
+    render();
+    window.requestAnimationFrame(raf);
+}
+
+raf();
 
 function Sizing() {
     width = window.innerWidth;
@@ -14,10 +61,4 @@ function Sizing() {
 
     canvas.height = height;
     canvas.width = width;
-}
-
-function raf() {
-    window.requestAnimationFrame(raf);
-}
-
-raf();
+};
